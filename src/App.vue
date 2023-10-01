f38ef4f9c4033569e755416f704ee6966e9f741e
caleb-cox <cacox25@gmail.com>
2023-10-01 14:48:30 -0400

    Add cards

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,18 @@
-<template>Hey</template>
+<script setup>
+import Card from "./components/Card.vue";
+</script>
+
+<template>
+  <div class="hand">
+    <Card v-for="_ in 10" />
+  </div>
+</template>
+
+<style scoped>
+.hand {
+  display: flex;
+  justify-content: center;
+  gap: 4rem;
+  flex-wrap: wrap;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,73 @@
+<script setup>
+const rollD6 = () => {
+  return Math.ceil(Math.random() * 6);
+};
+
+const cardObject = {
+  body: {
+    roll: [rollD6(), rollD6()],
+  },
+  mind: {
+    roll: [rollD6(), rollD6()],
+  },
+  luck: {
+    roll: [rollD6(), rollD6()],
+  },
+};
+
+const formatRoll = (dice) => {
+  const result = `${dice.roll[0] - dice.roll[1]}`;
+  return result.padStart(2, "+");
+};
+</script>
+
+<template>
+  <div class="card">
+    <div class="body stat">{{ formatRoll(cardObject.body) }}</div>
+    <div class="mind stat">{{ formatRoll(cardObject.mind) }}</div>
+    <div class="luck stat">{{ formatRoll(cardObject.luck) }}</div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  border: 0.125rem solid var(--color-white);
+  padding: 3rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+
+.stat {
+  font-size: 3rem;
+  line-height: 1;
+}
+
+.stat::after {
+  margin-left: 1rem;
+}
+
+.body.stat {
+  color: var(--color-rose);
+}
+
+.body.stat::after {
+  content: "\25A0";
+}
+
+.mind.stat {
+  color: var(--color-azure);
+}
+
+.mind.stat::after {
+  content: "\25B2";
+}
+
+.luck.stat {
+  color: var(--color-chartreuse);
+}
+
+.luck.stat::after {
+  content: "\25CF";
+}
+</style>
