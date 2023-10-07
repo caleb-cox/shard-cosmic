f67ea9a54529c1e060b9e8efe00df465b5a30146
caleb-cox <cacox25@gmail.com>
2023-10-07 13:49:08 -0400

    Change logic to weird 3d6 thing

--- src/App.vue
+++ src/App.vue
@@ -2,63 +2,26 @@
 import { ref } from "vue";
 import Card from "./components/Card.vue";
 
-const cards = ref({});
+const rolls = ref([]);
 
-const addCard = () => {
+const resetCard = () => {
   const roll = () => {
-    const d1 = Math.ceil(Math.random() * 6);
-    const d2 = Math.ceil(Math.random() * 6);
-    return d1 - d2;
+    return Math.ceil(Math.random() * 6);
   };
 
-  cards.value[crypto.randomUUID()] = {
-    body: roll(),
-    mind: roll(),
-    luck: roll(),
-  };
+  rolls.value = [roll(), roll(), roll()];
 };
 
-const deleteCard = (uuid) => {
-  delete cards.value[uuid];
-};
+resetCard();
 </script>
 
 <template>
   <h1>Shard Cosmic</h1>
-  <div class="hand">
-    <Card
-      v-for="(card, uuid) in cards"
-      :body="card.body"
-      :mind="card.mind"
-      :luck="card.luck"
-      @click="deleteCard(uuid)"
-    />
-  </div>
-  <button @click="addCard()">Add Card</button>
+  <Card :rolls="rolls" @click="resetCard()" />
 </template>
 
 <style scoped>
-button {
-  flex-shrink: 0;
-  height: 3rem;
-  width: 12.5rem;
-}
-
-.hand {
-  flex-shrink: 0;
-  border: 0.125rem solid var(--color-white);
-  padding: 2rem;
-  min-height: calc(0.125rem + 2rem + 17.5rem + 2rem + 0.125rem);
-  min-width: calc(0.125rem + 2rem + 12.5rem + 2rem + 0.125rem);
-  display: flex;
-  justify-content: center;
-  gap: 2rem;
-  flex-wrap: wrap;
-}
-
-@media (max-width: 40rem) {
-  .hand {
-    flex-direction: column;
-  }
+h1 {
+  margin-bottom: 1rem;
 }
 </style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,18 +1,18 @@
 <script setup>
-defineProps(["body", "mind", "luck"]);
+defineProps(["rolls"]);
 
 defineEmits(["deleteCard"]);
-
-const formatRoll = (result) => {
-  return result.toString().padStart(2, "+");
-};
 </script>
 
 <template>
   <div class="card">
-    <div class="stat body">{{ formatRoll(body) }}</div>
-    <div class="stat mind">{{ formatRoll(mind) }}</div>
-    <div class="stat luck">{{ formatRoll(luck) }}</div>
+    <div class="stat red">{{ rolls[0] }}</div>
+    <div class="stat yellow">{{ rolls[0] + rolls[1] }}</div>
+    <div class="stat green">{{ rolls[1] }}</div>
+    <div class="stat cyan">{{ rolls[1] + rolls[2] }}</div>
+    <div class="stat blue">{{ rolls[2] }}</div>
+    <div class="stat magenta">{{ rolls[0] + rolls[2] }}</div>
+    <div class="stat white">{{ rolls[0] + rolls[1] + rolls[2] }}</div>
   </div>
 </template>
 
@@ -20,45 +20,72 @@ const formatRoll = (result) => {
 .card {
   cursor: pointer;
   border: 0.125rem solid var(--color-white);
-  height: 17.5rem;
-  width: 12.5rem;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  gap: 1rem;
+  height: 21.5rem;
+  width: 21.5rem;
+  position: relative;
 }
 
 .stat {
-  font-size: 3rem;
-  line-height: 1;
+  position: absolute;
+  transform: translate(-50%, -50%);
+  border: 1px solid var(--color-white);
+  width: 2em;
+  height: 2em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  --radius: 3.5rem;
 }
 
-.stat::after {
-  margin-left: 1rem;
+.red {
+  left: 50%;
+  top: calc(50% - calc(var(--radius) * 2));
+  color: var(--color-red);
+  border-color: var(--color-red);
 }
 
-.body {
-  color: var(--color-rose);
+.yellow {
+  left: calc(50% - calc(var(--radius) * 1.732));
+  top: calc(50% - var(--radius));
+  color: var(--color-yellow);
+  border-color: var(--color-yellow);
+  font-size: 2rem;
 }
 
-.body::after {
-  content: "\25A0";
+.green {
+  left: calc(50% - calc(var(--radius) * 1.732));
+  top: calc(50% + var(--radius));
+  color: var(--color-green);
+  border-color: var(--color-green);
 }
 
-.mind {
-  color: var(--color-azure);
+.cyan {
+  left: 50%;
+  top: calc(50% + calc(var(--radius) * 2));
+  color: var(--color-cyan);
+  border-color: var(--color-cyan);
+  font-size: 2rem;
 }
 
-.mind::after {
-  content: "\25B2";
+.blue {
+  left: calc(50% + calc(var(--radius) * 1.732));
+  top: calc(50% + var(--radius));
+  color: var(--color-blue);
+  border-color: var(--color-blue);
 }
 
-.luck {
-  color: var(--color-chartreuse);
+.magenta {
+  left: calc(50% + calc(var(--radius) * 1.732));
+  top: calc(50% - var(--radius));
+  color: var(--color-magenta);
+  border-color: var(--color-magenta);
+  font-size: 2rem;
 }
 
-.luck::after {
-  content: "\25CF";
+.white {
+  left: 50%;
+  top: 50%;
+  font-size: 4rem;
 }
 </style>
